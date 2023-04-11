7c938a2fcc289b7b572b22e414dafd98e7260457
water38198 <46148965+water38198@users.noreply.github.com>
2023-04-11 15:19:42 +0800

    處理編譯後路徑問題

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,12 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+/* global process */
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/boardGameSellStation/",
+  base: process.env.NODE_ENV === "production" ? "/boardGameSellStation/" : "/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
