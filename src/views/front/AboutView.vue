439df271f891387cfcf1a80371e85c5a2a29673e
PayRoom <water38198@gmail.com>
2024-06-07 05:55:15 +0800

    AboutView 調整

--- src/views/front/AboutView.vue
+++ src/views/front/AboutView.vue
@@ -4,9 +4,9 @@
     <section>
       <ul class="list-unstyled ps-0 pb-4">
         <li data-aos="fade-up">
-          <div class="d-block d-md-flex">
-            <div class="w-sm-60 pe-md-4 align-self-center">
-              <h3>什麼是桌遊?</h3>
+          <div class="d-block d-md-flex justify-content-between align-items-center">
+            <div class="w-sm-60 pe-md-4">
+              <h3 class="mb-4 fs-1 text-center">什麼是桌遊?</h3>
               <p>
                 桌遊（Tabletop game or Board
                 game）是一種可以在桌面上進行的遊戲，通常需要使用遊戲板、棋子、骰子、卡牌、筆記本等工具。玩家們通常坐在桌子周圍進行遊戲，互相交流、競爭或合作。
@@ -14,38 +14,37 @@
                 同時，桌遊還可以培養玩家們的策略思考能力、創意能力、邏輯思維能力等等，並且可以提高玩家們的社交技能和團隊協作能力。
               </p>
             </div>
-            <div class="d-none d-md-block align-self-center">
+            <div class="d-none d-md-block">
               <img
                 src="https://images.unsplash.com/photo-1552985190-626af207368d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80" alt="dice image"/>
             </div>
           </div>
         </li>
         <li data-aos="fade-up">
-          <div class="d-block d-sm-flex flex-row-reverse my-5">
-            <div class="w-sm-60 ps-md-4 align-self-center">
-              <h3>為什麼建立桌遊販電?</h3>
-              <p>
-                我(PayRoom)熱衷於桌遊，不只玩桌桌遊也買了很多新的桌遊，無奈人的時間有限，
-                很多新的、幾乎沒碰過或者沒有機會打開來遊玩的桌遊只能在櫃子裡面積灰塵，對桌遊有愛的我，實在不忍心看他們的年華老去，希望能它們在下一位主人的手上發光發熱。
-              </p>
+          <div class="d-block d-sm-flex flex-row-reverse my-5 justify-content-between align-items-center">
+            <div class="w-sm-60 ps-md-4">
+              <h3 class="mb-4 fs-1 text-center">為什麼建立桌遊販電?</h3>
+              <ol class="ps-5">
+                <li class="mb-3"><span class="fw-bold">滿足市場需求</span>：桌遊販電提供一個專業的平台，滿足玩家對全新和二手桌遊的需求。</li>
+                <li class="mb-3"><span class="fw-bold">品質保證</span>：所有全新桌遊均來自正規渠道，確保品質。而二手桌遊經過詳細檢查和描述，讓玩家購買時無後顧之憂，享受物有所值的遊戲。</li>
+                <li class="mb-3"><span class="fw-bold">價格優勢</span>：二手桌遊的設置使玩家能以更實惠的價格獲得高質量的遊戲，特別是那些已經停產或難以找到的經典桌遊</li>
+                <li class="mb-3"><span class="fw-bold">推動桌遊文化</span>：我們希望推動桌遊文化的普及和發展，讓更多人能夠體驗到桌遊帶來的樂趣。</li>
+              </ol>
+              <p>桌遊販電致力於成為桌遊愛好者的購物平台，無論是全新桌遊還是二手桌遊，我們都希望通過我們的服務，讓每一位玩家都能找到自己喜愛的遊戲，享受快樂的遊戲時光。</p>
             </div>
-            <div class="d-none d-md-block align-self-center">
+            <div class="d-none d-md-block">
               <img src="https://images.unsplash.com/photo-1553481187-be93c21490a9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="a big dice"/>
             </div>
           </div>
         </li>
         <li data-aos="fade-up">
-          <div class="d-block d-sm-flex">
-            <div class="w-sm-60 pe-md-4 align-self-center">
-              <h3>桌遊販電提供什麼服務呢</h3>
-              <p>
-                桌遊販電會將二手桌遊以比市價便宜許多的價格出售，數量通常不多(1~2盒)，且會顯示商品本身的資訊(損傷、語言)，如果不介意二手商品的話，桌遊販電一定是您省錢的好選擇。
-              </p>
-              <p class="text-secondary">
-                註:二手桌遊指的是桌遊本身已經拆封且沒有留外膜，遊玩的次數不會超過3次
-              </p>
+          <div class="d-block d-sm-flex justify-content-between align-items-center">
+            <div class="w-sm-60 pe-md-4">
+              <h3 class="mb-4 fs-1 text-center">桌遊販電提供什麼服務呢</h3>
+              <p>桌遊販電提供種類繁多的全新桌遊，包括策略類、角色扮演類、合作類等，滿足不同玩家的需求。每款桌遊都經過嚴格篩選，保證品質，讓玩家能夠放心購買並享受高品質的遊戲體驗。</p>
+              <p>我們也特別設立了二手桌遊區，讓玩家可以以更實惠的價格購得心儀的遊戲。所有二手桌遊在上架前都會經過詳細檢查，確保遊戲配件齊全、狀況良好，並且我們會如實描述每一款二手商品的狀況，讓玩家購買時無後顧之憂。</p>
             </div>
-            <div class="d-none d-md-block align-self-center">
+            <div class="d-none d-md-block">
               <img src="https://images.unsplash.com/photo-1521790797524-b2497295b8a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80" alt="hand shaking"/>
             </div>
           </div>
@@ -56,7 +55,16 @@
 </template>
 
 <style scoped lang="scss">
+ul>li{
+  margin-bottom: 40px;
+}
+
 img{
   border-radius: 1rem;
+  max-height: 300px;
+}
+
+p{
+  line-height: 2;
 }
 </style>
