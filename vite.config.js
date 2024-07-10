9f313b9ba65ada7ef5c29ad950fbcd3ec3ae09d1
PayRoom <water38198@gmail.com>
2024-07-11 00:10:53 +0800

    vite config 修正

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,22 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  return{
   define: {
     __VUE_PROD_HYDRATION_MISMATCH_DETAILS__ : 'true',
   },
   plugins: [
     vue(),
-  ],
-  /* global process */
-  base: process.env.NODE_ENV === 'production' ? '/boardGameSellStation/' : '/',
+    ],
+  base:mode === 'production' ? '/boardGameSellStation/' : '/',
+  
+  // base: process.env.NODE_ENV === 'production' ? '/boardGameSellStation/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+}
+
+} )
